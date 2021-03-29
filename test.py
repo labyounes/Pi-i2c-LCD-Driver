1605738f14a73cfa302c0de60475385dd8b69fcb
lab1996 <labidi383@gmail.com>
2021-03-29 18:58:21 +0200

    test file

--- test.py
+++ test.py
@@ -4,4 +4,11 @@ from time import *
 lcd = lcddriver.lcd()
 
 #lcd.lcd_display_string("TEXT TO PRINT", ROW, COLUMN)
-lcd.lcd_display_string("Hello world", 1)
+
+lcd.lcd_display_string("Blog", 1)      #Show on display , 1.Row , 1.Column 
+
+lcd.lcd_display_string("BerryBase", 2) #Show on display , 2.Row , 1.Column 
+
+sleep(3)                               # 3 Seconds
+
+lcd.lcd_clear()                        # Clear display
