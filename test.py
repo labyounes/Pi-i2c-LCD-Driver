da85777479b5f0fdbcc42c9087f052a5bbfb55ab
lab1996 <labidi383@gmail.com>
2021-03-30 07:02:52 +0200

    last commit

--- test.py
+++ test.py
@@ -1,14 +1,22 @@
+#https://www.blog.berrybase.de/
+
+import sys
+import os 
 import lcddriver
 from time import *
 
+#Lcd Inisialization 
 lcd = lcddriver.lcd()
 
-#lcd.lcd_display_string("TEXT TO PRINT", ROW, COLUMN)
 
-lcd.lcd_display_string("Blog", 1)      #Show on display , 1.Row , 1.Column 
+#lcd.lcd_display_string("TEXT TO PRINT", LINE, COLUMN)
 
-lcd.lcd_display_string("BerryBase", 2) #Show on display , 2.Row , 1.Column 
+lcd.lcd_display_string("Blog",1)            #Display on , 1.line , 1.column
+    
+lcd.lcd_display_string("BerryBase",2)       #Display on , 2.line , 1.column
 
-sleep(3)                               # 3 Seconds
+sleep(3)                                    #3 Seconds 
 
-lcd.lcd_clear()                        # Clear display
+lcd.lcd_clear()                             #Clear Display 
+
+sys.exit()                                  #Finish Program 
